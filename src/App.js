31e45d67fbfbdca0cd39869039caa21cde0fcdcb
Prathamjeet Singh <47675131+IshuSingh1@users.noreply.github.com>
2024-01-03 22:00:31 -0500

    socials added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ProfileImage from './components/ProfileImage';
+import SocialButtons from './components/SocialButtons';
 import './App.css'; // Make sure this file contains the gradient styles
 
 function App() {
   return (
-    <div className="gradient-background">
-      <header className="App-header">
-        <ProfileImage />
-        <h1 className="h1-animated-gradient">Hello, I'm Ishu Singh</h1>
-        {/* ... the rest of your content ... */}
-      </header>
+    <div className="App">
+      {/* Fixed container for social buttons */}
+      <div className="fixed-container">
+        <SocialButtons />
+      </div>
+
+      {/* Scrollable content */}
+      <div className="gradient-background scrollable-container">
+        <header className="App-header">
+          <ProfileImage />
+          <h1 className="h1-animated-gradient">Hello, I'm Ishu Singh</h1>
+          {/* ... other components that will be scrolled into view ... */}
+        </header>
+      </div>
     </div>
   );
 }
--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FaLinkedin, FaGithub } from 'react-icons/fa'; // Using react-icons for simplicity
 
+const linkedinLink = "https://www.linkedin.com/in/prathamjeet-singh-0041781b4/";
+const githubLink = "https://github.com/IshuSingh1";
 const SocialButtons = () => {
   return (
     <div className="social-buttons">
-      <a href="https://www.linkedin.com/in/your-linkedin" target="_blank" rel="noopener noreferrer" className="social-button">
+      <a href={linkedinLink} target="_blank" rel="noopener noreferrer" className="social-button">
         <FaLinkedin size={30} />
       </a>
-      <a href="https://www.github.com/your-github" target="_blank" rel="noopener noreferrer" className="social-button">
+      <a href={githubLink} target="_blank" rel="noopener noreferrer" className="social-button">
         <FaGithub size={30} />
       </a>
     </div>
